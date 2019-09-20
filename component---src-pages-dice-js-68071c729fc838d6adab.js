2d01b61093a66dbadee588ab21a0de7f6e41db1c
Kestutis Balcius <kestutis_balcius@hotmail.com>
2019-09-20 13:47:38 +0200

    Updates

---
+++ component---src-pages-dice-js-68071c729fc838d6adab.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[40],{254:function(e,t,a){"use strict";a.r(t);var n=a(2),l=a.n(n),r=a(101),c=a(215),d=a(214);t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(d.a,{title:"Vad betyder dice?"}),l.a.createElement("div",{className:"content-header"},l.a.createElement("h1",null,"Vad betyder dice?"),l.a.createElement("p",null,"Engelskt ord för tärning."),l.a.createElement(r.a,{to:"/",className:"back-button"},"Tillbaka till startsidan")))}}}]);
+//# sourceMappingURL=component---src-pages-dice-js-68071c729fc838d6adab.js.map
---
+++ component---src-pages-odds-js-225b07f92e6109982865.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[108],{322:function(e,a,t){"use strict";t.r(a);var n=t(2),l=t.n(n),r=t(101),c=t(215),s=t(214);a.default=function(){return l.a.createElement(c.a,null,l.a.createElement(s.a,{title:"Vad är odds?"}),l.a.createElement("div",{className:"content-header"},l.a.createElement("h1",null,"Vad är odds?"),l.a.createElement("p",null,"Proportionen mellan insats och vinst."),l.a.createElement(r.a,{to:"/",className:"back-button"},"Tillbaka till startsidan")))}}}]);
+//# sourceMappingURL=component---src-pages-odds-js-225b07f92e6109982865.js.map
