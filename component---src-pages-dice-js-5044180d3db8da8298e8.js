ad35bc5b514b4edd765d5a45adcf047aff822cba
Kestutis Balcius <kestutis_balcius@hotmail.com>
2019-09-20 17:30:20 +0200

    Updates

---
+++ component---src-pages-dice-js-5044180d3db8da8298e8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[41],{255:function(e,t,a){"use strict";a.r(t);var n=a(2),l=a.n(n),r=a(101),c=a(215),d=a(214);t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(d.a,{title:"Vad betyder dice?"}),l.a.createElement("div",{className:"content-header"},l.a.createElement("h1",null,"Vad betyder dice?"),l.a.createElement("p",null,"Engelskt ord för tärning."),l.a.createElement(r.a,{to:"/",className:"back-button"},"Tillbaka till startsidan")))}}}]);
+//# sourceMappingURL=component---src-pages-dice-js-5044180d3db8da8298e8.js.map
---
+++ component---src-pages-quads-js-2a22123af04553c60b19.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[127],{341:function(e,a,t){"use strict";t.r(a);var n=t(2),l=t.n(n),r=t(101),c=t(215),d=t(214);a.default=function(){return l.a.createElement(c.a,null,l.a.createElement(d.a,{title:"Vad betyder quads?"}),l.a.createElement("div",{className:"content-header"},l.a.createElement("h1",null,"Vad betyder quads?"),l.a.createElement("p",null,"Annat ord för fyrtal."),l.a.createElement(r.a,{to:"/",className:"back-button"},"Tillbaka till startsidan")))}}}]);
+//# sourceMappingURL=component---src-pages-quads-js-2a22123af04553c60b19.js.map
