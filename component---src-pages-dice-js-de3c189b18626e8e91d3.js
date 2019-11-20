2f6d8a84191b900e6b81ae42fada458f987d8e08
Kestutis Balcius <kestutis_balcius@hotmail.com>
2019-11-20 13:04:01 +0100

    Updates

---
+++ component---src-pages-dice-js-de3c189b18626e8e91d3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[42],{256:function(e,t,a){"use strict";a.r(t);var n=a(2),l=a.n(n),r=a(101),c=a(215),d=a(214);t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(d.a,{title:"Vad betyder dice?"}),l.a.createElement("div",{className:"content-header"},l.a.createElement("h1",null,"Vad betyder dice?"),l.a.createElement("p",null,"Engelskt ord för tärning."),l.a.createElement(r.a,{to:"/",className:"back-button"},"Tillbaka till startsidan")))}}}]);
+//# sourceMappingURL=component---src-pages-dice-js-de3c189b18626e8e91d3.js.map
---
+++ component---src-pages-huset-js-956ad66a9c7919152763.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[72],{286:function(e,t,a){"use strict";a.r(t);var n=a(2),l=a.n(n),r=a(101),c=a(215),s=a(214);t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(s.a,{title:"Vad betyder huset?"}),l.a.createElement("div",{className:"content-header"},l.a.createElement("h1",null,"Vad betyder huset?"),l.a.createElement("p",null,"Annat ord för casinot."),l.a.createElement(r.a,{to:"/",className:"back-button"},"Tillbaka till startsidan")))}}}]);
+//# sourceMappingURL=component---src-pages-huset-js-956ad66a9c7919152763.js.map
